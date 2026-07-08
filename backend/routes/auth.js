870e1f316ccca140062a9813801d4a9055ea1923
Jaden Simmons <jadensimmons1803@gmail.com>
2026-07-08 19:20:39 -0400

    updated import in auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const authController = require('../controllers/authController');
 const { register, login } = require('../controllers/authController');
 
 router.post('/register', register);
