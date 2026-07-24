bf69c9bf0a28611188737ec0d6a870f41e08db4b
Augustskib <Augustskibnes@gmail.com>
2026-07-24 16:06:28 -0400

    Add DELETE endpoints for categories and cards

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const authenticate = require('../middleware/auth');
-const { getCards, createCard } = require('../controllers/cardController');
-
+const { getCards, createCard, deleteCard } = require('../controllers/cardController');
 router.use(authenticate);
-
 router.get('/', getCards);
 router.post('/', createCard);
-
+router.delete('/:id', deleteCard);
 module.exports = router;
